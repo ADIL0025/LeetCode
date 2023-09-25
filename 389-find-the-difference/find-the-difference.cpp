b449a382bafc4d7de83a66f432040a77a44299f7
Adil Ahmad <91419013+ADIL0025@users.noreply.github.com>
2023-09-25 19:43:28 +0530

    Time: 5 ms (25.17%) | Memory: 7.2 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,14 +1,9 @@
 class Solution {
- public:
-  char findTheDifference(string s, string t) {
-    char ans = 0;
-
-    for (const char c : s)
-      ans ^= c;
-
-    for (const char c : t)
-      ans ^= c;
-
-    return ans;
-  }
+public:
+    char findTheDifference(string s, string t) {
+        char c = 0;
+        for(char cs : s) c ^= cs;
+        for(char ct : t) c ^= ct;
+        return c;
+    }
 };
