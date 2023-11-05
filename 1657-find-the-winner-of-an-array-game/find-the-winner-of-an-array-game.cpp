2e398b5beb05b90df960253de86c4ca0c76ef884
Adil Ahmad <91419013+ADIL0025@users.noreply.github.com>
2023-11-05 23:55:37 +0530

    Time: 79 ms (84.72%) | Memory: 63.4 MB (86.11%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -1,17 +1,25 @@
 class Solution {
- public:
-  int getWinner(vector<int>& arr, int k) {
-    int ans = arr[0];
-    int wins = 0;
-
-    for (int i = 1; i < arr.size() && wins < k; ++i)
-      if (arr[i] > ans) {
-        ans = arr[i];
-        wins = 1;
-      } else {
-        ++wins;
-      }
-
-    return ans;
-  }
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int n = arr.size();
+        if (k >= n) {
+            return *max_element(arr.begin(), arr.end());
+        }
+        
+        int currentMax = arr[0];
+        int wins = 0;
+        int i = 1;
+        
+        while (wins < k) {
+            if (arr[i] > currentMax) {
+                currentMax = arr[i];
+                wins = 1;
+            } else {
+                wins++;
+            }
+            i = (i + 1) % n;
+        }
+        
+        return currentMax;
+    }
 };
